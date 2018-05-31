aab2ada67c78a5712a2b1af1ddfe082cc71cad61
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-05-31 03:32:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,11 @@
 class Application
 
+  @@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
   end
 
 end

53d0a3964042545f4e718d7e02bbfe5111eda96c
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-05-31 03:40:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,15 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    if req.path.match(/items/)
+      item_name = req.path.split(/items/).last
+      if item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.status = 400 
+        resp.write "Item not found"
+      end
+    end
   end
 
 end
